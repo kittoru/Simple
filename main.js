30f9fd450f463161afe12da8cefd4a41bd072665
Kittoru <mama190774@gmail.com>
2024-07-03 17:23:55 +0300

    first version

---
+++ main.js
@@ -0,0 +1,28 @@
+
+const search = document.querySelector('.search');
+const input = document.querySelector('.header__item-form');
+const inputClose = document.querySelector('.header__input-close');
+
+const buttonOpen = document.querySelector(".menu__mobile");
+const buttonClose = document.querySelector('.header__item-close');
+const menu = document.querySelector('.header__list');
+const shadow = document.querySelector('.shadow');
+
+
+search.addEventListener('click', ()=> {
+  input.classList.add('open');
+});
+
+inputClose.addEventListener('click', ()=> {
+  input.classList.remove('open');
+});
+
+buttonOpen.addEventListener('click', ()=> {
+  menu.classList.add('mobile__list');
+  shadow.classList.add('open');
+});
+
+buttonClose.addEventListener('click', ()=> {
+  menu.classList.remove('mobile__list');
+  shadow.classList.remove('open');
+});
